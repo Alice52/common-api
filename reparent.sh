fa13ccdd91af5ba00e76860f859a8af6a09502af
zack.zhang <zackzhang@augmentum.com.cn>
2022-09-07 18:18:42 +0800

    fix: change parent artifact & version

--- reparent.sh
+++ reparent.sh
@@ -4,8 +4,11 @@ rm pom.xml
 mv pom.xml.dev pom.xml
 
 ls -r
-pname=$(basename $(dirname $(dirname "$PWD")))
+# pname=$(basename $(dirname "$PWD"))
+pname=$( cat ./../pom.xml | grep \<\/artifactId\> -m 1 | cut -d'>' -f2 | cut -d'<' -f1 |  tr -d ' ')
+echo pname=${pname}
 sed -i "s/P_ARTIFACTID/${pname}/g" $(grep P_ARTIFACTID -rl ./pom.xml)
 
-pversion=$(cat pom.xml | grep \<\/version\> -m 1 | cut -d'<' -f1 | tr -d ' ')
+pversion=$( cat ./../pom.xml | grep \<\/version\> -m 1 | cut -d'>' -f2 | cut -d'<' -f1 |  tr -d ' ')
+echo pversion=${pversion}
 sed -i "s/P_VERSION/${pversion}/g" $(grep P_VERSION -rl ./pom.xml)
