2c5eb3f04fc44cfabf7b5f1d878b87f1e1eb9b23
Alice52 <zzhang_xz@163.com>
2022-10-28 22:12:59 +0800

    upgrade mybatis version

--- common-database/database-mybatis-plus/src/main/java/common/database/config/MybatisPlusConfigure.java
+++ common-database/database-mybatis-plus/src/main/java/common/database/config/MybatisPlusConfigure.java
@@ -3,13 +3,12 @@ package common.database.config;
 import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import common.database.config.handler.MybatisMetaHandler;
-import common.database.interceptor.DataScopeInterceptor;
 import common.database.interceptor.DeSensitiveFieldInterceptor;
 import common.database.interceptor.QuerySensitiveInterceptor;
 import common.database.interceptor.SensitiveFieldInterceptor;
 import common.database.plugin.SensitivePlugin;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,25 +29,25 @@ public class MybatisPlusConfigure extends BaseMybatisConfig {
         return globalConfig;
     }
 
-    /**
-     * 分页插件
-     *
-     * @return PaginationInterceptor
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
-    }
+    //    /**
+    //     * 分页插件
+    //     *
+    //     * @return PaginationInterceptor
+    //     */
+    //    @Bean
+    //    public PaginationInterceptor paginationInterceptor() {
+    //        return new PaginationInterceptor();
+    //    }
 
     /**
      * 数据权限插件
      *
      * @return DataScopeInterceptor
      */
-    @Bean
-    public DataScopeInterceptor dataScopeInterceptor() {
-        return new DataScopeInterceptor();
-    }
+    //    @Bean
+    //    public DataScopeInterceptor dataScopeInterceptor() {
+    //        return new DataScopeInterceptor();
+    //    }
 
     /**
      * 默认逻辑删除
